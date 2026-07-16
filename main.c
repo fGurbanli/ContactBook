6c84b107b0b60758ded10991287edcdbf3a7cd11
Furgan Gurbanli <f.qurbanli.2006@gmail.com>
2026-07-16 11:38:40 +0400

    added initial functions

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Contacts {
+    char name[20];
+    int phone;
+    char email[20];
+
+};
+
+void printMenu();
+void AddContact();
+void ListContact();
+void DeleteContact();
+int SearchContact();
 
 int main(void) {
-    printf("Hello, World!\n");
+    int count = 5;
+    int* contSize = malloc(count * sizeof(int));
+    struct Contacts contacts[*contSize];
+
+
+
+
+
+    free(contSize);
     return 0;
 }
